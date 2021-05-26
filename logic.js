d2ab251531b4a5d8ae7ca23447d55a4c7dd25d98
Anurag1972 <anurag.jay1002@gmail.com>
2021-05-26 09:52:21 +0000

    added reset-color-button

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
-    document.addEventListener("DOMContentLoaded",function()
-            {
+document.addEventListener("DOMContentLoaded",function()
+           {
             //free response checker
             document.querySelector("#check").addEventListener("click",function(){
 
@@ -9,25 +9,41 @@
                 answerform.style.backgroundColor="green";
                 document.querySelector("#feedback2").innerHTML="correct!";
             }
+
+         
             else
             {
                 answerform.style.backgroundColor="red";
                 document.querySelector("#feedback2").innerHTML="Incorrect";
             }
+
              });
+
+let incorrect=document.querySelectorAll("#incorrect");
+
+
+            // will reset the colors of all button
+             function reset_button_color(){
+                 document.querySelector("#correct").style.backgroundColor = "#d9edff";
+                for(let i=0;i<incorrect.length;i++){
+                    incorrect[i].style.backgroundColor ="#d9edff";
+                }
+             }
              // MCQ checker
                  //for correct answer
              document.querySelector("#correct").addEventListener("click",function(){
+                 reset_button_color();
                 document.querySelector("#correct").style.backgroundColor="green";
                 document.querySelector("#feedback1").innerHTML="Correct!";
              });
                //for incorrect answer
-                  let incorrect=document.querySelectorAll("#incorrect");
                   for(let i=0;incorrect.length;i++)
                   {
                       incorrect[i].addEventListener("click",function(){
+                          reset_button_color();
                          incorrect[i].style.backgroundColor="red";
                          document.querySelector("#feedback1").innerHTML="Incorrect";
+
                       });
                   }
       });
